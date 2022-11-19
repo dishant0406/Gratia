b53788fb1e2f266aecad54dac67271313fdb07aa
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2022-11-19 12:34:13 +0530

    ...

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -10,7 +10,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use('/', routes);
+app.use('/api', routes);
 
 app.listen(port, () => {
     console.log(`Server started on port http://localhost:${port}`);
